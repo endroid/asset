75e0f11b073d51ed408b788b10479047326812e0
endroid <info@endroid.nl>
2018-05-28 19:28:34 +0000

    Fix unit test

--- tests/Factory/AssetFactoryTest.php
+++ tests/Factory/AssetFactoryTest.php
@@ -10,19 +10,23 @@
 namespace Endroid\Asset\Tests\Factory;
 
 use Endroid\Asset\DataAsset;
+use Endroid\Asset\Factory\Adapter\DataAssetFactoryAdapter;
 use Endroid\Asset\Factory\AssetFactory;
+use Endroid\Asset\Guesser\ClassGuesser;
+use PHPUnit\Framework\TestCase;
 
 class AssetFactoryTest extends TestCase
 {
-    public function testFactory()
+    public function testFactory(): void
     {
-        $assetFactory = new AssetFactory();
-        $assetFactory->addFactory(new DataAssetFactory());
+        $assetFactory = new AssetFactory(new ClassGuesser());
+        $assetFactory->addFactory(new DataAssetFactoryAdapter());
 
-        $asset = $assetFactory->create(DataAsset::class, [
+        $asset = $assetFactory->create(null, [
             'data' => 'Asset data'
         ]);
 
         $this->assertInstanceOf(DataAsset::class, $asset);
+        $this->assertEquals($asset->getData(), 'Asset data');
     }
 }
