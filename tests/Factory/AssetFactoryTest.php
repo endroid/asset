4d06845c3447837f607b56142067137bb58f0e0c
endroid <info@endroid.nl>
2018-05-28 19:55:20 +0000

    Update unit test

--- tests/Factory/AssetFactoryTest.php
+++ tests/Factory/AssetFactoryTest.php
@@ -12,14 +12,13 @@ namespace Endroid\Asset\Tests\Factory;
 use Endroid\Asset\DataAsset;
 use Endroid\Asset\Factory\Adapter\DataAssetFactoryAdapter;
 use Endroid\Asset\Factory\AssetFactory;
-use Endroid\Asset\Guesser\ClassGuesser;
 use PHPUnit\Framework\TestCase;
 
 class AssetFactoryTest extends TestCase
 {
     public function testFactory(): void
     {
-        $assetFactory = new AssetFactory(new ClassGuesser());
+        $assetFactory = new AssetFactory();
         $assetFactory->addFactory(new DataAssetFactoryAdapter());
 
         $asset = $assetFactory->create(null, [
