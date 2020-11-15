9a31e50c44f3f86201fadbf2b61834d642910860
Jeroen van den Enden <info@endroid.nl>
2020-11-15 19:12:32 +0000

    Extend stringable

--- src/AssetInterface.php
+++ src/AssetInterface.php
@@ -2,16 +2,9 @@
 
 declare(strict_types=1);
 
-/*
- * (c) Jeroen van den Enden <info@endroid.nl>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace Endroid\Asset;
 
-interface AssetInterface
+interface AssetInterface extends \Stringable
 {
     public function getData(): string;
 }
